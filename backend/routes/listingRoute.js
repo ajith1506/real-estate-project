536a7dcfc917aaab4a116da99c9c831ec2d7a377
ajith1506 <ajithkumararun1111@gmail.com>
2024-10-21 20:19:19 +0530

    changes

--- backend/routes/listingRoute.js
+++ backend/routes/listingRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/create", verifyToken, listingController.createListing);
 router.delete("/delete/:id", verifyToken, listingController.deleteListing);
 router.put("/update/:id", verifyToken, listingController.updateListing);
-router.get("/get/:id", listingController.getListing);
+router.get("/get/:id", verifyToken, listingController.getListing);
 router.get("/get", listingController.getListings);
 
 module.exports = router;
