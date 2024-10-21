c23275165e8d8f66994874bd4d7ae1b05e62f924
ajith1506 <ajithkumararun1111@gmail.com>
2024-10-21 18:47:07 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://ak15-e-commerce.netlify.app"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"], // Add other headers if needed
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
